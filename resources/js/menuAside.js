f30680cad844789179aa376f79c2799b10469091
omarwnasser <82316429+omarwnasser@users.noreply.github.com>
2023-10-05 00:33:47 +0300

    laravel vue one dashboard

---
+++ resources/js/menuAside.js
@@ -0,0 +1,35 @@
+import {
+  mdiAccountCircle,
+  mdiMonitor,
+  mdiGithub,
+  mdiLock,
+  mdiAlertCircle,
+  mdiSquareEditOutline,
+  mdiTable,
+  mdiViewList,
+  mdiTelevisionGuide,
+  mdiResponsive,
+  mdiPalette,
+  mdiReact
+} from '@mdi/js'
+
+
+export default [
+  
+    {
+      route: 'dashboard',
+      icon: mdiMonitor,
+      label: 'Dashboard'
+    },
+    // {
+    //   route: "another-route-name",
+    //   icon: mdiMonitor,
+    //   label: "Dashboard 2",
+    // },
+    {
+      href: 'https://example.com/',
+      icon: mdiMonitor,
+      label: 'Example.com'
+    }
+  
+]
